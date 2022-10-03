1d482cf96473b2981cc777db20deb94ad30a0d2e
Zahid Zaman <zahid_zaman81@hotmail.com>
2022-10-03 14:38:54 +1300

    Update streamlit_app.py
    Code fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas, streamlit
 
-streamlit.title('Healthy Dinner')	my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.header('Breakfast Favorites')	streamlit.dataframe(my_fruit_list)
+streamlit.title('Healthy Dinner')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega3 & Blueberry Oatmeal')	
 streamlit.text('Kale, Spinach & Rocket Smoothie')	
 streamlit.text('Hard-Boiled Free-Range Egg')	
