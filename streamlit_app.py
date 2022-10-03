a642e62eedee5c8d16d43476b9f49dff4ec5439b
Zahid Zaman <zahid_zaman81@hotmail.com>
2022-10-03 14:38:12 +1300

    Update streamlit_app.py
    Add Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import pandas, streamlit
+
+streamlit.title('Healthy Dinner')	my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.header('Breakfast Favorites')	streamlit.dataframe(my_fruit_list)
+streamlit.text('Omega3 & Blueberry Oatmeal')	
+streamlit.text('Kale, Spinach & Rocket Smoothie')	
+streamlit.text('Hard-Boiled Free-Range Egg')	
+streamlit.header('Build Your Own Smoothie')	
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
