d03ae9ab21e97e28fa7d3061f36de689582af731
Zahid Zaman <zahid_zaman81@hotmail.com>
2022-10-03 14:44:55 +1300

    Update streamlit_app.py
    Add default

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.header('Build Your Own Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
