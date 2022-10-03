a3d92eeeb8942da78609f6cbe667b380aad25e40
Zahid Zaman <zahid_zaman81@hotmail.com>
2022-10-03 14:35:43 +1300

    Update streamlit_app.py
    Read data using panda from S3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
-import streamlit
-streamlit.title('Healthy Dinner')
-streamlit.header('Breakfast Favorites')
-streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('Build Your Own Smoothie')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
