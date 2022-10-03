59229545c486a6221ac434df15ae5fe12ab57fbc
Zahid Zaman <zahid_zaman81@hotmail.com>
2022-10-03 14:33:20 +1300

    Update streamlit_app.py
    add header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('Healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Build Your Own Smoothie')
