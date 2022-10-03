e60b3f923b75176aad1bf3b17d440b6c43b5064e
Zahid Zaman <zahid_zaman81@hotmail.com>
2022-10-03 14:42:41 +1300

    Update streamlit_app.py
    update index column

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,6 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('Build Your Own Smoothie')	
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
